072185c370e518d75fc5b137dadc4647f327c0eb
Eugene Dorfman <eugene.dorfman@gmail.com>
2025-05-05 17:15:24 +0200

    dotnet run -p:Platform=x64

--- ci/generate-accessors.ps1
+++ ci/generate-accessors.ps1
@@ -7,8 +7,11 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 Push-Location $RepoName/PropertyGenerator
 try {
-    dotnet build -c Release
-    dotnet run $DataFile ..
+    # TODO: remove after IpIntelligence properly fixes .targets file 
+    # otherwise without passing this argument it tries to infer win-x86 
+    # from the IpIntelligence package and fails
+    # it is supposed to build on ubuntu-latest x64, so this should work
+    dotnet run -p:Platform=x64 $DataFile ..
 } finally {
     Pop-Location
 }
