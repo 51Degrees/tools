0ad4ff2701ef4deffe0883ea3bd312c3881b6ea5
Alexander Popel <alexander.popel@postindustria.com>
2026-01-13 17:19:35 +0200

    PropertyGenerator: use CopyrightUpdater's copyright text
    To avoid having them in two different places.

--- ci/generate-accessors.ps1
+++ ci/generate-accessors.ps1
@@ -1,20 +1,18 @@
 param (
-    [Parameter(Mandatory)][string]$RepoName,
     [Parameter(Mandatory)][string]$DataType,
-    [Parameter(Mandatory)][string]$MetaDataPath
+    [Parameter(Mandatory)][string]$MetaDataPath,
+    $RepoName # unused, here for compatibility
 )
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 
-Push-Location $RepoName/PropertyGenerator
+Push-Location "$PSScriptRoot/../PropertyGenerator"
 try {
-    # TODO: remove after IpIntelligence properly fixes .targets file 
-    # otherwise without passing this argument it tries to infer win-x86 
+    # TODO: remove -p after IpIntelligence properly fixes .targets file
+    # otherwise without passing this argument it tries to infer win-x86
     # from the IpIntelligence package and fails
     # it is supposed to build on ubuntu-latest x64, so this should work
-    dotnet build -c:Release -p:Platform=x64
-
-    dotnet "$(find ./bin -name PropertyGenerator.dll | head -n 1)" "$DataType" .. "$MetaDataPath"
+    dotnet run -c:Release -p:Platform=x64 -- "$DataType" .. "$MetaDataPath"
 } finally {
     Pop-Location
 }
