22050b7dd44a381139b8e4c5b2310e382a81731e
Alexander Popel <alexander.popel@postindustria.com>
2026-01-15 09:35:09 +0200

    ci: fix arch parameter in generate-accessors

--- ci/generate-accessors.ps1
+++ ci/generate-accessors.ps1
@@ -8,11 +8,11 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 Push-Location "$PSScriptRoot/../PropertyGenerator"
 try {
-    # TODO: remove -p after IpIntelligence properly fixes .targets file
+    # TODO: remove -a after IpIntelligence properly fixes .targets file
     # otherwise without passing this argument it tries to infer win-x86
     # from the IpIntelligence package and fails
     # it is supposed to build on ubuntu-latest x64, so this should work
-    dotnet run -c:Release -p:Platform=x64 -- "$DataType" .. "$MetaDataPath"
+    dotnet run -c:Release -a:x64 -- "$DataType" .. "$MetaDataPath"
 } finally {
     Pop-Location
 }
